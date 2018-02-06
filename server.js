afd15bfd247a03ff7fc49cfc007efdb5ac7fbded
Filip Stangricki <stangricki@gmail.com>
2018-02-06 16:41:53 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/nodeappdatabase', {
+    useMongoClient: true
+});
+
+//new user Schema
+const userSchema = new Schema({
+    name: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    admin: Boolean,
+    created_at: Date,
+    updated_at: Date
+});
+
+//Mongoose schema method
+userSchema.methods.manify = function(next) {
+    this.name = this.name + '-boy';
+
+    return next(null, this.name);
+};
+
+//pre-save method
+userSchema.pre('save', function(next) {
+    //pobranie aktualnego czasu
+    const currentDate = new Date();
+
+    //zmiana pola na aktualny czas
+    this.updated_at = currentDate;
+
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
+});
