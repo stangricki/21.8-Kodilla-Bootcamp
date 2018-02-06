610864ff37a638199e88b48b2cc9c2362fa64b15
Filip Stangricki <stangricki@gmail.com>
2018-02-06 17:12:25 +0100

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://stangricki:database-1@ds125388.mlab.com:25388/database-1', {
     useMongoClient: true
 });
 
