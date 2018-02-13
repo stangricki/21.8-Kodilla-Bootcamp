7bf0bef70068964e4def06af05d9758de056b4b1
Filip Stangricki <stangricki@gmail.com>
2018-02-13 20:39:52 +0100

    Back to the begining

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(_dirname + '/public'))
+
 app.get('/'), (req, res) => {res.send('Hello world')}
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
