ee7d58ab987651278ce8989aa0bb85e94aec9956
Filip Stangricki <stangricki@gmail.com>
2018-02-13 20:30:16 +0100

    Back to the begining

--- server.js
+++ server.js
@@ -1,38 +1,4 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var express = require('express');
+var app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://stangricki:database-1@ds125388.mlab.com:25388/database-1', {
-    useMongoClient: true
-});
-
-//new user Schema
-const userSchema = new Schema({
-    name: String,
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    admin: Boolean,
-    created_at: Date,
-    updated_at: Date
-});
-
-//Mongoose schema method
-userSchema.methods.manify = function(next) {
-    this.name = this.name + '-boy';
-
-    return next(null, this.name);
-};
-
-//pre-save method
-userSchema.pre('save', function(next) {
-    //pobranie aktualnego czasu
-    const currentDate = new Date();
-
-    //zmiana pola na aktualny czas
-    this.updated_at = currentDate;
-
-    if (!this.created_at)
-        this.created_at = currentDate;
-
-    next();
-});
+app.get('/'), (req, res) => {res.send('Hello world')}
