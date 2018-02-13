e86d22dacf0b2466381af4af0ee3411f17a3c4a8
Filip Stangricki <stangricki@gmail.com>
2018-02-13 21:14:30 +0100

    Back to the begining

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://stangricki:password-1@ds229388.mlab.com:29388/database-1', {
     useMongoClient: true
 });
 
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/views'));
 
-app.get('/', function(req, res) {
+app.get('/',(req, res) => {
     res.render('index');
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
     console.log('Our app is running on http://localhost:' + port);
 });
